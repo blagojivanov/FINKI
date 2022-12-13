5a8b18a3e876d5a4285aa5e4618852c1476fb904
Blagoj <blagoj.ivanov@yahoo.com>
2022-12-13 13:11:56 +0100

    added 2-3

---
+++ cmake-build-debug/CMakeFiles/2-3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "2-3.exe"
+  "2-3.exe.manifest"
+  "2-3.pdb"
+  "CMakeFiles/2-3.dir/SP/Vezbi_II_Kol/2-3.c.obj"
+  "CMakeFiles/2-3.dir/SP/Vezbi_II_Kol/2-3.c.obj.d"
+  "lib2-3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
