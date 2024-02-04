b6ac19b0afe811f355488f951386ff2cf0577214
Blagoj <blagoj.ivanov@yahoo.com>
2024-02-04 11:29:45 +0100

    np second midterm/exam telco test design patterns update

--- NP/src/io/github/biv2101/VezbiVtor/TelcoTest2.java
+++ NP/src/io/github/biv2101/VezbiVtor/TelcoTest2.java
@@ -3,18 +3,7 @@ package io.github.biv2101.VezbiVtor;
 //package mk.ukim.finki.midterm;
 
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
-
-enum Action {
-    ANSWER, HOLD, RESUME, END
-}
-
-enum State {
-    IDLE, RINGING, PROGRESS, PAUSED
-}
 
 class DurationConverter {
     public static String convert(long duration) {
@@ -24,21 +13,161 @@ class DurationConverter {
     }
 }
 
-class StateP {
-    private State state;
-    private long timestamp;
+enum Action {
+    ANSWER, HOLD, END, RESUME
+}
 
-    public long getTimestamp() {
-        return timestamp;
+interface IState {
+    void hold(long timestamp);
+
+    void answer(long timestamp);
+
+    void resume(long timestamp);
+
+    void end(long timestamp);
+}
+
+abstract class State implements IState {
+    Call call;
+
+    public State(Call call) {
+        this.call = call;
+    }
+}
+
+class RingingState extends State {
+
+    public RingingState(Call call) {
+        super(call);
     }
 
-    public StateP(State state, long timestamp) {
-        this.state = state;
-        this.timestamp = timestamp;
+    @Override
+    public void hold(long timestamp) {
+        throw new RuntimeException();
+
     }
 
-    public State getState() {
-        return state;
+    @Override
+    public void answer(long timestamp) {
+        call.startTimestamp = timestamp;
+        call.state = new InProgressState(call);
+    }
+
+    @Override
+    public void resume(long timestamp) {
+        throw new RuntimeException();
+
+    }
+
+    @Override
+    public void end(long timestamp) {
+        call.startTimestamp = timestamp;
+        call.endTimestamp = timestamp;
+        call.duration = 0;
+    }
+}
+
+class InProgressState extends State {
+
+    public InProgressState(Call call) {
+        super(call);
+    }
+
+    @Override
+    public void hold(long timestamp) {
+        call.holdTimestamp = timestamp;
+        call.state = new PausedState(call);
+    }
+
+    @Override
+    public void answer(long timestamp) {
+        throw new RuntimeException();
+
+    }
+
+    @Override
+    public void resume(long timestamp) {
+        throw new RuntimeException();
+
+    }
+
+    @Override
+    public void end(long timestamp) {
+        call.endTimestamp = timestamp;
+        if (call.holdTimestamp != -1)
+        {
+            call.totalHoldtime += timestamp - call.holdTimestamp;
+        }
+        call.duration = call.endTimestamp - call.totalHoldtime - call.startTimestamp;
+
+        call.state = new IdleState(call);
+    }
+}
+
+class PausedState extends State {
+
+    public PausedState(Call call) {
+        super(call);
+    }
+
+    @Override
+    public void hold(long timestamp) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void answer(long timestamp) {
+        throw new RuntimeException();
+
+    }
+
+    @Override
+    public void resume(long timestamp) {
+        call.totalHoldtime += timestamp - call.holdTimestamp;
+        call.holdTimestamp = -1;
+        call.state = new InProgressState(call);
+    }
+
+    @Override
+    public void end(long timestamp) {
+        call.endTimestamp = timestamp;
+        if (call.holdTimestamp != -1)
+        {
+            call.totalHoldtime += timestamp - call.holdTimestamp;
+        }
+        call.duration = call.endTimestamp - call.totalHoldtime - call.startTimestamp;
+
+        call.state = new IdleState(call);
+    }
+}
+
+class IdleState extends State {
+
+    public IdleState(Call call) {
+        super(call);
+    }
+
+    @Override
+    public void hold(long timestamp) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void answer(long timestamp) {
+        throw new RuntimeException();
+
+    }
+
+    @Override
+    public void resume(long timestamp) {
+        throw new RuntimeException();
+
+    }
+
+    @Override
+    public void end(long timestamp) {
+        throw new RuntimeException();
+
     }
 }
 
@@ -48,88 +177,51 @@ class Call {
     private String reciever;
     private long timestamp;
 
-    private List<StateP> states;
+    //timetracking
+    long startTimestamp;
+    long totalHoldtime = 0;
+    long holdTimestamp = -1;
+    long endTimestamp;
 
+    long duration = 0;
+    State state = new RingingState(this);
 
     public Call(String uuid, String dialer, String reciever, long timestamp) {
         this.uuid = uuid;
         this.dialer = dialer;
         this.reciever = reciever;
         this.timestamp = timestamp;
-        states = new ArrayList<>();
-        states.add(new StateP(State.RINGING, timestamp));
+        this.startTimestamp = timestamp;
     }
 
     public void update(String action, long timestamp) {
         Action enumerated = Action.valueOf(action);
-        if (states.get(states.size() - 1).getState() == State.IDLE) {
-            if (enumerated == Action.ANSWER) {
-                states.add(new StateP(State.RINGING, timestamp));
-            }
-        } else if (states.get(states.size() - 1).getState() == State.RINGING) {
-            if (enumerated == Action.ANSWER) {
-                states.add(new StateP(State.PROGRESS, timestamp));
-            } else if (enumerated == Action.END) {
-                states.add(new StateP(State.IDLE, timestamp));
-            }
-        } else if (states.get(states.size() - 1).getState() == State.PROGRESS) {
-            if (enumerated == Action.HOLD) {
-                states.add(new StateP(State.PAUSED, timestamp));
-            } else if (enumerated == Action.END) {
-                states.add(new StateP(State.IDLE, timestamp));
-            }
-        } else if (states.get(states.size() - 1).getState() == State.PAUSED) {
-            if (enumerated == Action.RESUME) {
-                states.add(new StateP(State.PROGRESS, timestamp));
-            } else if (enumerated == Action.END) {
-                states.add(new StateP(State.IDLE, timestamp));
-            }
+        if (enumerated == Action.ANSWER) {
+            state.answer(timestamp);
+        } else if (enumerated == Action.END) {
+            state.end(timestamp);
+        } else if (enumerated == Action.HOLD) {
+            state.hold(timestamp);
+        } else if (enumerated == Action.RESUME) {
+            state.resume(timestamp);
         }
-    }
-
-    public StateP callStart() {
-
-        if (callDuration() == 0) {
-            return states.stream().filter(x -> x.getState() == State.IDLE).findFirst().orElse(null);
-        }
-        return states.stream().filter(x -> x.getState() == State.PROGRESS).findFirst().orElse(null);
-    }
-
-    public StateP callEnd() {
-        if (callDuration() == 0) {
-            return new StateP(State.IDLE, 0);
-        }
-        return states.stream().filter(x -> x.getState() == State.IDLE).findFirst().orElse(null);
-    }
-
-    public long callDuration() {
-        Queue<StateP> stateq = new LinkedList<>(states);
-
-        long totalDuration = 0;
-        while (!stateq.isEmpty()) {
-            StateP curr = stateq.poll();
-            if (curr.getState() == State.PROGRESS) {
-                StateP next = stateq.poll();
-                totalDuration += next.getTimestamp() - curr.getTimestamp();
-            } else if (curr.getState() == State.RINGING) {
-                if (stateq.peek().getState() == State.IDLE) {
-                    return 0;
-                }
-            }
-        }
-        return totalDuration;
     }
 
     public String getDialer() {
         return dialer;
     }
+
     public String getReciever() {
         return reciever;
     }
+
     public long getTimestamp() {
         return timestamp;
     }
 
+    public long getDuration() {
+        return duration;
+    }
 }
 
 class TelcoApp {
@@ -150,23 +242,23 @@ class TelcoApp {
     void printChronologicalReport(String phoneNumber) {
         calls.values().stream().filter(x -> x.getDialer().equals(phoneNumber) || x.getReciever().equals(phoneNumber))
                 .sorted(Comparator.comparing(Call::getTimestamp)).forEach(x -> System.out.printf("%s %s %d %s %s\n", x.getReciever().equals(phoneNumber) ? "R" : "D",
-                        x.getReciever().equals(phoneNumber) ? x.getDialer() : x.getReciever(), x.callStart().getTimestamp(), x.callEnd().getTimestamp() != 0 ? x.callEnd().getTimestamp() : "MISSED CALL", DurationConverter.convert(x.callDuration())));
+                        x.getReciever().equals(phoneNumber) ? x.getDialer() : x.getReciever(), x.startTimestamp, x.endTimestamp != x.startTimestamp ? x.endTimestamp : "MISSED CALL", DurationConverter.convert(x.duration)));
     }
 
     void printReportByDuration(String phoneNumber) {
         calls.values().stream().filter(x -> x.getDialer().equals(phoneNumber) || x.getReciever().equals(phoneNumber))
-                .sorted(Comparator.comparing(Call::callDuration).thenComparing(Call::getTimestamp).reversed()).forEach(x -> System.out.printf("%s %s %d %s %s\n", x.getReciever().equals(phoneNumber) ? "R" : "D",
-                        x.getReciever().equals(phoneNumber) ? x.getDialer() : x.getReciever(), x.callStart().getTimestamp(), x.callEnd().getTimestamp() != 0 ? x.callEnd().getTimestamp() : "MISSED CALL", DurationConverter.convert(x.callDuration())));
+                .sorted(Comparator.comparing(Call::getDuration).thenComparing(Call::getTimestamp).reversed()).forEach(x -> System.out.printf("%s %s %d %s %s\n", x.getReciever().equals(phoneNumber) ? "R" : "D",
+                        x.getReciever().equals(phoneNumber) ? x.getDialer() : x.getReciever(), x.startTimestamp, x.endTimestamp != x.startTimestamp ? x.endTimestamp : "MISSED CALL", DurationConverter.convert(x.duration)));
     }
 
     void printCallsDuration() {
         Map<String, Long> callMap = new TreeMap<>();
 
         for (Call call : calls.values()) {
-            callMap.put(call.getDialer() + " <-> " + call.getReciever(), calls.values().stream().filter(x -> x.getDialer().equals(call.getDialer()) && x.getReciever().equals(call.getReciever())).mapToLong(Call::callDuration).sum());
+            callMap.put(call.getDialer() + " <-> " + call.getReciever(), calls.values().stream().filter(x -> x.getDialer().equals(call.getDialer()) && x.getReciever().equals(call.getReciever())).mapToLong(Call::getDuration).sum());
         }
         Comparator<Map.Entry<String, Long>> c = Comparator.comparing(Map.Entry::getValue);
-        callMap.entrySet().stream().sorted(c.reversed()).forEach(x->System.out.printf("%s : %s\n", x.getKey(), DurationConverter.convert(x.getValue())));
+        callMap.entrySet().stream().sorted(c.reversed()).forEach(x -> System.out.printf("%s : %s\n", x.getKey(), DurationConverter.convert(x.getValue())));
     }
 }
 
